2e80af46607a0fd2312c892d862a340015641666
ashutosh1401 <ashutoshthakur_14@yahoo.com>
2020-09-22 23:06:33 +0530

    Added Timestamps

--- models/post.js
+++ models/post.js
@@ -22,7 +22,7 @@ const postSchema = new mongoose.Schema({
     type: ObjectId,
     ref: "User",
   },
-});
+}, { timestamps: true });
 
 const Post = mongoose.model("Post", postSchema);
 module.exports = Post;
